6ddbcb44614c0999814b332c9cbebec154301305
Naman Jain <nj1411@ic.ac.uk>
2015-05-08 10:52:32 +0100

    modified do_mds_all_individuals.sh

--- do_mds_all_individuals.sh
+++ do_mds_all_individuals.sh
@@ -1,9 +1,9 @@
-plink --bfile perperson_and_snp_and_maf --genome --out mdsall
 
+if [ ! -f mdsall.genome ]; then
+	plink --bfile perperson_and_snp_and_maf --genome --out mdsall
+fi
 
-# Here I am doing a 2 dimensional
-plink --bfile mydata --read-genome mdsall.genome --cluster --mds-plot 2
-
-
+# Here I am doing a 2 dimensional mds. 
+plink --bfile perperson_and_snp_and_maf --read-genome mdsall.genome --cluster --mds-plot 2 --out mdsall
 
 
